84495c155569230b08973cf0cba0dbe50ea9d213
rmcinto <rodger@geekshake.com>
2018-01-08 11:49:18 -0700

    Fixed issue with the repo checkout async

--- cli.js
+++ cli.js
@@ -6,16 +6,32 @@
 //load the required modules
 var cmdArgs = require('trujs-cmdargs')(process.argv)
 , compile = require('./index.js')
+, compileReporter = compile(".compileReporter")
 //resolve the compiler worker from the compile container
 , run = compile('.run')
 , saver = compile('.saver')
-, compileReporter = compile(".compileReporter")
 ;
 
-compileReporter.addHandler(function (msg, type) {
-    console.log(msg);
-});
 compileReporter.setLevels("all");
+compileReporter.addHandler(function (msg, type) {
+    if (type.indexOf("group") === -1) {
+        if (type === "error") {
+            console.error(msg);
+        }
+        else if (type === "warning") {
+            console.warn(msg);
+        }
+        else {
+            console.log(msg);
+        }
+    }
+    else if (type === "group") {
+        console.group();
+    }
+    else if (type === "groupEnd") {
+        console.groupEnd();
+    }
+});
 
 console.log("*****************************************************");
 console.log("Starting the compiler");
--- scripts/collector/_ModuleCollector.js
+++ scripts/collector/_ModuleCollector.js
@@ -118,7 +118,7 @@ function _ModuleCollector(promise, getScriptsDir, defaults, pathParser, nodePath
     entry.return = defaults.entry.module.return;
 
     //set the repositories to the required branches
-    var proc = type_module_checkoutRepositories(base, entry);
+    var proc = promise.resolve();
 
     //get the module file data
     proc = proc.then(function () {
